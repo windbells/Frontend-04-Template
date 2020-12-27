acd9bfedb55d3eedef83c1134d373ecb20b35304
huangqina <huangqina1231@qq.com>
2020-12-27 22:09:06 +0800

    提交week17 第5课，集成generator

---
+++ Week_17/generator-toytool/app/templates/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>{{greeting}}</p>
+</template>
+
+<script>
+module.default = {
+  data: function () {
+    return {
+      greeting: 'Hello',
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Week_17/generator-toytool/app/webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack'); // 用于访问内置插件
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const config = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      { test: /\.vue$/, use: 'vue-loader', },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env"],
+          }
+        },
+
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/*.html", to: "[name].[ext]" },
+      ],
+    }),
+  ]
+};
+
+module.exports = config;
